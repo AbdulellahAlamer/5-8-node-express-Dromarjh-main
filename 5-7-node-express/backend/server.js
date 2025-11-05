51b6e0589d6754cab3abdc2bf93b0e60002a4a48
Abdulellah Alamer <122591461+AbdulellahAlamer@users.noreply.github.com>
2025-11-05 09:23:07 +0300

    todo 1

--- 5-7-node-express/backend/server.js
+++ 5-7-node-express/backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
+// TODO: write code for cors
 
+// TODO: write code to define routes
 
-{/*write code to define routes*/}
+const app = express();
+const PORT = 3000;
 
-
-{/*write code to create server*/}
-
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

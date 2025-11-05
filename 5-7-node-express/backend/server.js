0ede09d49b2f907b195ac9ed23074a9b7ea93f77
Abdulellah Alamer <122591461+AbdulellahAlamer@users.noreply.github.com>
2025-11-05 09:30:16 +0300

    todo 5

--- 5-7-node-express/backend/server.js
+++ 5-7-node-express/backend/server.js
@@ -9,6 +9,15 @@ app.use(cors());
 
 // TODO: write code to define routes
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quotes API!");
+});
+
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
